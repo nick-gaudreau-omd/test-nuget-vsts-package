e82595dc09b3e8dfa65f014de905d16bc5d0afdb
Nick Gaudreau <nick.gaudreau@ontariomd.com>
2018-06-19 13:59:46 -0400

    Simple class library project

---
+++ VstsPackage/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VstsPackage
+{
+    public class Class1
+    {
+        public static string Get()
+        {
+            return "Vsts Package Test Class1 Get()";
+        }
+    }
+}
